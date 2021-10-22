da364e4fc15e0bd6d45364d3642047dbe11940fe
soundy-geek <92983169+soundy-geek@users.noreply.github.com>
2021-10-22 16:36:04 +0530

    Update Highly divisible triangle number.py

--- Highly divisible triangle number.py
+++ Highly divisible triangle number.py
@@ -1,4 +1,6 @@
-print("Task: Find the value of the first triangle number to have over 'N' divisors")
+'''
+Task: Find the value of the first triangle number to have over 'N' divisors")
+'''
 div = int(input("Value for N: "))
 n = 0
 i = 0
@@ -7,7 +9,7 @@ while True:
     n += i
     divisors = [j for j in range(1, n+1) if n%j == 0]
     if len(divisors) > div:
-        print(n,"is the first triangle number to have over",div,"divisors")
+        print(n, "is the first triangle number to have over", div, "divisors")
         break
     else:
         divisors.clear()
